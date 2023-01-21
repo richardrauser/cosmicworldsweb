ca087e3a12956de26294d9ecdfc03173a159956d
Richard Rauser <richard@raaza.net>
2023-01-21 08:31:46 +0000

    basic index

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Alien Worldz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Alien Worldz" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          On-chain, generative NFT art offering glimpses into alien worlds.
         </p>
       </main>
 
