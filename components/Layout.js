765909fce6a8b5991a4cd7381ffb7fbe9ee1d67a
Richard Rauser <richard@raaza.net>
2023-04-12 15:58:40 -0400

    metadata tags for OG and Twitter

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,26 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import 'react-toastify/dist/ReactToastify.css';
 import React from "react";
+import Head from 'next/head';
 
 export default function Layout(props) {
     const { children } = props;
 
     return (
         <div className="layout">
+            <Head>
+                <title>Cosmic Worlds</title>
+                <meta name="twitter:card" content="Cosmic Worlds - alien landscapes on the Ethereum blockchain!" />
+                <meta name="twitter:site" content="@volstrate"/>
+                <meta name="twitter:title" content="Cosmic Worlds - alien landscapes on the Ethereum blockchain!" />
+                <meta name="twitter:description" content="On-chain, generative cosmic worlds on the Ethereum blockchain." />
+                <meta name="twitter:creator" content="@volstrate"/>
+                <meta name="twitter:image" content="http://cosmicworlds.xyz/images/cosmicWorld.png" />
+                <meta property="og:title" content="Cosmic Worlds - alien landscapes on the Ethereum blockchain!" />
+                <meta property="og:url" content="http://cosmicworlds.xyz/" />
+                <meta property="og:image" content="http://cosmicworlds.xyz/images/cosmicWorld.png" />
+                <meta property="og:description" content="Description Here" />
+            </Head>
             <CosmicNavBar/>
             <ToastContainer />
 
