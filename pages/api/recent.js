5def61d9b0147c18a096c6ab0a3e50d531027f5f
Richard Rauser <richard@raaza.net>
2023-04-13 12:02:59 -0400

    correcting deployment bug, margin around about images

--- pages/api/recent.js
+++ pages/api/recent.js
@@ -1,17 +1,17 @@
-import { CosmicWorldsContractAddress } from '../../utils/Constants';
-import {Alchemy, Network} from '@alch/alchemy-web3';
+// import { CosmicWorldsContractAddress } from '../../utils/Constants';
+// import {Alchemy, Network} from '@alch/alchemy-web3';
 
-export default async function handler(req, res) {
-  const settings = {
-    apiKey: process.env['ALCHEMY_API_KEY'],
-    network: Network.ETH_MAINNET,
-  };
-  const alchemy = new Alchemy(settings);
+// export default async function handler(req, res) {
+//   const settings = {
+//     apiKey: process.env['ALCHEMY_API_KEY'],
+//     network: Network.ETH_MAINNET,
+//   };
+//   const alchemy = new Alchemy(settings);
 
-  const allNfts = await alchemy.nft.getNftsForContract(CosmicWorldsContractAddress);
-  const supply = allNfts.nfts.length;
+//   const allNfts = await alchemy.nft.getNftsForContract(CosmicWorldsContractAddress);
+//   const supply = allNfts.nfts.length;
 
-  var recentNfts = (supply <= 5) ? allNfts.nfts : allNfts.nfts.slice(supply - 5);
+//   var recentNfts = (supply <= 5) ? allNfts.nfts : allNfts.nfts.slice(supply - 5);
 
-  res.status(200).json({ totalSupply: supply, recent: recentNfts });
-}
+//   res.status(200).json({ totalSupply: supply, recent: recentNfts });
+// }
