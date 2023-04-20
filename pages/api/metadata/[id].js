ae27b8ee19260b830a8132a301777bf443a2b90a
Richard Rauser <richard@raaza.net>
2023-04-20 16:00:52 +0100

    commenting out write operations to fs

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -5,9 +5,9 @@ export default async function handler(req, res) {
     try {
         const tokenId = req.query.id;
         const dir = "metadata";
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
+        // if (!fs.existsSync(dir)) {
+        //     fs.mkdirSync(dir);
+        // }
         const fileName = dir + "/" + tokenId + ".json";
 
         let details;
@@ -19,8 +19,8 @@ export default async function handler(req, res) {
         } else {
             console.log("File not found with name: " + fileName + " Reading from blockchain...");
             details = await fetchTokenDetails(tokenId);
-            console.log("Writing token details to file: " + fileName);
-            fs.writeFileSync(fileName, JSON.stringify(details));
+            // console.log("Writing token details to file: " + fileName);
+            // fs.writeFileSync(fileName, JSON.stringify(details));
         }
 
         res.status(200).json({ 
