0f87ba78f9ba58e0834e5a828725693c29c64c2f
Richard Rauser <richard@raaza.net>
2023-04-20 15:58:25 +0100

    creating metadata dir if it doesn't exist

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -4,7 +4,11 @@ import fs from "fs";
 export default async function handler(req, res) {
     try {
         const tokenId = req.query.id;
-        const fileName = "metadata/" + tokenId + ".json";
+        const dir = "metadata";
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        const fileName = dir + "/" + tokenId + ".json";
 
         let details;
 
@@ -15,8 +19,8 @@ export default async function handler(req, res) {
         } else {
             console.log("File not found with name: " + fileName + " Reading from blockchain...");
             details = await fetchTokenDetails(tokenId);
-            // console.log("Writing token details to file: " + fileName);
-            // fs.writeFileSync(fileName, JSON.stringify(details));
+            console.log("Writing token details to file: " + fileName);
+            fs.writeFileSync(fileName, JSON.stringify(details));
         }
 
         res.status(200).json({ 
