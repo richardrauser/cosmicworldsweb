69020d640373bb5dff89e943cf09080f1233ec67
Richard Rauser <richard@raaza.net>
2023-04-20 10:35:08 +0100

    File system cache for metadata

--- pages/api/metadata/[id].js
+++ pages/api/metadata/[id].js
@@ -1,13 +1,24 @@
 import { fetchTokenDetails } from 'utils/BlockchainAPI';
-import { fetchRecentTokenIds, fetchTotalSupply } from '../../../utils/BlockchainAPI';
-import { useRouter } from 'next/router';
+import fs from "fs";
 
 export default async function handler(req, res) {
-    const tokenId = req.query.id;
-
     try {
-        const details = await fetchTokenDetails(tokenId);
+        const tokenId = req.query.id;
+        const fileName = "public/metadata/" + tokenId + ".json";
 
+        let details;
+
+        if (fs.existsSync(fileName)) {
+            console.log("Found file with name: " + fileName);
+            const file = fs.readFileSync(fileName);
+            details = JSON.parse(file);
+        } else {
+            console.log("File not found with name: " + fileName + " Reading from blockchain...");
+            details = await fetchTokenDetails(tokenId);
+            fs.writeFileSync(fileName, JSON.stringify(details));
+        }
+
+        console.log("Returning token details: " + details);
         res.status(200).json({ 
             tokenDetails: details
         });    
