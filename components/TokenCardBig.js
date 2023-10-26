62f61090e4d8c80b05d541a99aca485ed06ba0f7
Richard Rauser <richard@raaza.net>
2023-10-26 19:06:44 +0100

    fixing spelling

--- components/TokenCardBig.js
+++ components/TokenCardBig.js
@@ -32,7 +32,7 @@ export default function TokenCardBig(props) {
         setLoading(false);
         setSvg(svg);
         setTokenSvgDataUri(svgDataUri);
-        setTraitsText(`Seed: ${seed}, planet count: ${planetCount}, stars: ${starDensity}, mountains: ${mountainRoughness}, water: ${waterChoppiness}, clounds: ${cloudType}`);
+        setTraitsText(`Seed: ${seed}, planet count: ${planetCount}, stars: ${starDensity}, mountains: ${mountainRoughness}, water: ${waterChoppiness}, clouds: ${cloudType}`);
 
         } catch (error) {
           console.log("Error occurred fetching token metadata: ", error);
